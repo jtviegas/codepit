97d9b2bdedecdcdf3700161292129554a7b17b41
jtviegas <jtviegas.js@google.com>
2016-11-19 20:18:16 +0000

    ...

--- assorted/microservices/swaggerexp/api/controllers/hello.js
+++ assorted/microservices/swaggerexp/api/controllers/hello.js
@@ -24,21 +24,38 @@ var util = require('util');
   In the starter/skeleton project the 'get' operation on the '/hello' path has an operationId named 'hello'.  Here,
   we specify that in the exports of this module that 'hello' maps to the function named 'hello'
  */
-module.exports = {
-  hello: hello
-};
 
-/*
-  Functions in a127 controllers used for operations should take two parameters:
 
-  Param 1: a handle to the request object
-  Param 2: a handle to the response object
- */
-function hello(req, res) {
-  // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
-  var name = req.swagger.params.name.value || 'stranger';
-  var hello = util.format('Hello, %s!', name);
+var Hello = function(){
 
-  // this sends back a JSON response which is a single string
-  res.json(hello);
-}
+    var getTwo = function(req, res, next) {
+        var id = req.swagger.params.id.value;
+        var result = {"ok": true, "result": {"id": id}};
+        res.json(result);
+    };
+
+    var getOne = function(req, res, next) {
+        var id = req.swagger.params.id.value;
+        var result = {"ok": true, "result": {"id": id}};
+        res.json(result);
+    };
+
+    var del = function(req, res, next) {
+        var id = req.swagger.params.id.value;
+        var result = {"ok": true, "result": {"id": id}};
+        res.json(result);
+    };
+
+    var post = function(req, res, next) {
+        var msg = req.body;
+        var result = {"ok": true, "result": {"msg": msg}};
+        res.json(result);
+    };
+
+    return { getTwo:getTwo, getOne:getOne, del:del, post:post };
+}();
+
+module.exports = Hello;
+
+
+
--- assorted/microservices/swaggerexp/init.sh
+++ assorted/microservices/swaggerexp/init.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+
+PROJ_NAME=swaggerexp
+
 npm install -g swagger
-swagger project create swaggerexp
-swagger project start swaggerexp
+swagger project create $PROJ_NAME
+
+# load editor
+swagger project edit
+# running in mock mode
+swagger project start $PROJ_NAME -m
+# running in ral mode
+swagger project start $PROJ_NAME
