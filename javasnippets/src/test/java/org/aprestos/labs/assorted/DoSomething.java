3ed3ef9e65e5ef0cfeac119b98a212a3057e36f1
jtviegas <jtviegas.js@gmail.com>
2018-08-13 00:03:12 +0100

    ...

--- javasnippets/src/test/java/org/aprestos/labs/assorted/DoSomething.java
+++ javasnippets/src/test/java/org/aprestos/labs/assorted/DoSomething.java
@@ -4,9 +4,132 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.IntStream;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public final class DoSomething {
+
+	@Test
+	public void test_00() {
+
+		int[][] u = new int[][] { { 4, 8, 2 }, { 4, 5, 7 }, { 6, 1, 6 } };
+		int[][] u2 = new int[][] { { 4, 8, 2, 1, 3 }, { 4, 5, 7, 8, 4 }, { 6, 1, 6, 5, 1 }, { 4, 5, 7, 1, 2 },
+				{ 6, 1, 6, 4, 9 } };
+
+		Assert.assertArrayEquals(rotate(u), new int[][] { { 2, 7, 6 }, { 8, 5, 1 }, { 4, 4, 6 } });
+		Assert.assertArrayEquals(rotate(u2), new int[][] { { 3, 4, 1, 2, 9 }, { 1, 8, 5, 1, 4 }, { 2, 7, 6, 7, 6 },
+				{ 8, 5, 1, 5, 1 }, { 4, 4, 6, 4, 6 } });
+
+		Assert.assertEquals(findEdgeCentralOne(new int[][] { { 2, 1, 6 }, { 8, 5, 1 }, { 4, 4, 6 } }), 0);
+		Assert.assertEquals(findEdgeCentralOne(new int[][] { { 2, 3, 6 }, { 8, 5, 1 }, { 4, 4, 6 } }), 3);
+		Assert.assertEquals(findEdgeCentralOne(u2), 3);
+
+		Assert.assertArrayEquals(transform2MagicSquare(new int[][] { { 4, 8, 2 }, { 4, 5, 7 }, { 6, 6, 1 } }),
+				new int[][] { { 4, 1, 2 }, { 4, 5, 7 }, { 6, 6, 8 } });
+
+		Assert.assertArrayEquals(transform2MagicSquare(new int[][] { { 4, 8, 2 }, { 4, 5, 1 }, { 6, 6, 2 } }),
+				new int[][] { { 2, 1, 2 }, { 8, 5, 6 }, { 4, 4, 6 } });
+
+	}
+
+	private int[][] transform2MagicSquare(int[][] m) {
+
+		int orientation = findEdgeCentralOne(m);
+		if (0 > orientation) // there is no startPoint
+			m = findValueAndSwapIt(1, m.length / 2, m);
+		else if (0 < orientation)
+			m = rotateNtimes(4 - orientation, m);
+
+		return m;
+
+	}
+
+	private int[][] findValueAndSwapIt(int val, int pos, int[][] m) {
+		boolean found = false;
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m.length; j++) {
+				if (val == m[i][j]) {
+					found = true;
+					int x = pos / m.length;
+					int y = pos % m.length;
+					int val0 = m[x][y];
+					m[x][y] = val;
+					m[i][j] = val0;
+					break;
+				}
+			}
+		}
+		if (!found)
+			throw new IllegalArgumentException("couldn't find the value to swap position");
+
+		return m;
+	}
+
+	private int findEdgeCentralOne(int[][] o) {
+
+		int r = -1;
+		int n = o.length;
+
+		int endIndex = n - 1;
+		if (1 == o[0][n / 2]) {
+			r = 0; // north
+		} else if (1 == o[n / 2][0]) {
+			r = 1; // west
+		} else if (1 == o[endIndex][n / 2]) {
+			r = 2; // south
+		} else if (1 == o[n / 2][endIndex]) {
+			r = 3; // east
+		}
+
+		return r;
+	}
+
+	private int[][] copy(int[][] o) {
+		int[][] r = new int[o.length][o[0].length];
+
+		for (int i = 0; i < o.length; i++) {
+			for (int j = 0; j < o.length; j++) {
+				r[i][j] = o[i][j];
+			}
+		}
+
+		return r;
+	}
+
+	private int[][] rotateNtimes(int n, int[][] m) {
+		for (int i = 0; i < n; i++) {
+			m = rotate(m);
+		}
+		return m;
+	}
+
+	private int[][] rotate(int[][] m) {
+		int n = m[0].length;
+		int[][] d = copy(m);
+		for (int i = 0; i < n / 2; i++) {
+			rotateLayer(i, m, d);
+		}
+		return d;
+	}
+
+	private int[][] rotateLayer(int layer, int[][] source, int[][] destin) {
+
+		int len = source.length;
+
+		int startIndex = layer;
+		int endIndex = len - startIndex - 1;
+
+		for (int i = startIndex, j = endIndex; i <= endIndex; i++, j--) {
+			destin[j][startIndex] = source[startIndex][i]; // N -> W
+			destin[endIndex][j] = source[j][startIndex]; // W -> S
+			destin[i][endIndex] = source[endIndex][j]; // S -> E
+			destin[startIndex][i] = source[i][endIndex]; // E -> N
+		}
+
+		return destin;
+	}
+
+	// #########################################################################
 
 	public Integer sol(String s) {
 
@@ -69,43 +192,6 @@ public final class DoSomething {
 			sample[j] = i;
 
 		int lineValue = (n * (n ^ 2 + 1)) / 2;
-
-		return r;
-	}
-
-	@Test
-	public void test_00() {
-
-		int[][] u = new int[][] { { 4, 8, 2 }, { 4, 5, 7 }, { 6, 1, 6 } };
-
-		// System.out.println(rotate90(u));
-
-	}
-
-	private int[][] copy(int[][] o) {
-		int[][] r = new int[o.length][o[0].length];
-
-		for (int i = 0; i < o.length; i++) {
-			for (int j = 0; j < o.length; j++) {
-				r[i][j] = o[i][j];
-			}
-		}
-
-		return r;
-	}
-
-	private int[][] rotate(int[][] m) {
-		int n = m[0].length;
-		int[][] r = new int[m.length][m[0].length];
-		for (int i = 0; i < n / 2; i++) {
-			r = rotateLayer(i, m);
-		}
-		return r;
-	}
-
-	private int[][] rotateLayer(int layer, int[][] m) {
-
-		int[][] r = copy(m);
 
 		return r;
 	}
