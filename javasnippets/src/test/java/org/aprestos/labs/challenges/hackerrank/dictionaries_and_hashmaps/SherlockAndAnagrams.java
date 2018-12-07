0657c1ac70dff9cf25bf5cd95bad8d750508ab67
joao viegas <jviegas_contractor@decaresystems.ie>
2018-12-07 17:37:13 +0000

    ...

--- javasnippets/src/test/java/org/aprestos/labs/challenges/hackerrank/dictionaries_and_hashmaps/SherlockAndAnagrams.java
+++ javasnippets/src/test/java/org/aprestos/labs/challenges/hackerrank/dictionaries_and_hashmaps/SherlockAndAnagrams.java
@@ -1,11 +1,6 @@
 package org.aprestos.labs.challenges.hackerrank.dictionaries_and_hashmaps;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.Arrays;
 
 import org.junit.Assert;
 import org.junit.FixMethodOrder;
@@ -21,170 +16,53 @@ import org.junit.runners.MethodSorters;
 public class SherlockAndAnagrams {
 
   @Test
-	public void test() throws Exception {
+	public void test1() throws Exception {
 		Assert.assertEquals(4, solution("abba"));
 	}
-  
-  public static int solution2(String s) {
-    
-    int result = 0;
-    char[] text = s.toCharArray();
-    
-    final char[] repeatingChars = findRepeatingChars(text) ;
-    int anagramMaxLength = s.length()-1;
-    
-    for(int i=0; i < anagramMaxLength; i++) {
-      for(int j=0, k = s.length()-1; j < k ; j++, k--) {
-        char[] left = new char[i+1];
-        char[] right = new char[i+1];
-        System.arraycopy(text, 0, left, 0, i+1);
-        System.arraycopy(text, k, right, 0, i+1);
-
-        if( madeOfChars(repeatingChars, left) && madeOfChars(repeatingChars, right) ) {
-          result += isAnagram(left, right) ? 1 : 0;
-        }
-      }
-    }
-    
-    return result;
+  @Test
+  public void test2() throws Exception {
+    Assert.assertEquals(10, solution("kkkk"));
+  }
+  @Test
+  public void test3() throws Exception {
+    Assert.assertEquals(3, solution("ifailuhkqq"));
   }
   
+ 
   public static int solution(String s) {
-    
-    int result = 0;
-    
-    bruteforce(s);
-    
-    return result;
-  }
-  
-  private static boolean inArray(char c, char[] text) {
-    boolean r = false;
-    for(char o: text) {
-      if( o == c ) {
-        r = true;
-        break;
-      }
-    }
-    return r;
-  }
-  
-  private static int bruteforce(String s) {
     int r = 0;
-    char[] text = s.toCharArray();
-    // get repeating chars
-    final char[] repeatingChars = findRepeatingChars(text) ;
-    // find sequences of repeating chars
-    List<String> sequences = new ArrayList<String>();
-    int pointer = -1;
-    for( int i=0; i < s.length();i++ ) {
-      if( inArray(text[i], repeatingChars)  ) {
-        if (-1 == pointer) 
-          pointer = i;
-      }  
-      else {
-        if (-1 < pointer) {
-          sequences.add(s.substring(pointer, i));
-          pointer = -1;
+    int anagramMaxLength = s.length()-1;
+    for(int i=1; i<=anagramMaxLength; i++) {
+      int anagramLength = i;
+      for(int j=0; j+anagramLength<s.length(); j++) {
+        String toMatch = s.substring(j, j+anagramLength);
+        for(int k=j+1; k+anagramLength < s.length() + 1;k++) {
+          String matcher = s.substring(k,k+anagramLength);
+          if( hasSameChars(toMatch.toCharArray(), matcher.toCharArray()) )
+            r++;
         }
       }
-        
     }
-    
-    if (-1 < pointer) {
-      sequences.add(s.substring(pointer));
-    }
-    // for every sequence of repeating find all combinations of chars
-    Map<Integer, Set<String>> combinations = new HashMap<>();
-    for(int i=1; i<text.length; i++) {
-      for(int j=0;j+i<=text.length;j++) {
-        if( !combinations.containsKey(i) ) combinations.put(i, new HashSet<String>());
-        combinations.get(i).add(s.substring(j, j+i));
-      }
-    }
-    // go through the sets and find pairs of anagrams
-    
-   
     return r;
   }
-  
-  static boolean madeOfChars(char[] universe, char[] test) {
-    boolean result = true;
-    for(char u : test) {
-      boolean found=false;
-      for (char c: universe)
-        if (c == u)
-          found=true;
-      if(!found) {
-        result = false;
-        break;
-      }
-      
-    }
-    return result;
-  }
-  
 
   
-  static Set<String> computePermutations(char[] permutation, char[] variables, int limitLength) {
+  private static boolean hasSameChars(char[] a, char[] b){
     
-    final Set<String> permutations = new HashSet<String>();
-
-    for(char o: variables) {
-      String newPermutation = new String(addToCharArray(o, permutation));
-      if( newPermutation.length() < limitLength) 
-        permutations.addAll(computePermutations(newPermutation.toCharArray(), variables, limitLength));
-      permutations.add(newPermutation);
-    }
-
-    return permutations;
-  }
-  
-  static char[] addToCharArray(char c, char[] a) {
-    char[] result = new char[a.length+1];
-    System.arraycopy(a, 0, result, 0, a.length);
-    result[a.length] = c;
-    return result;
-  }
-  
-  static Object[] addToArray(Object c, Object[] a) {
-    Object[] result = new Object[a.length+1];
-    System.arraycopy(a, 0, result, 0, a.length);
-    result[a.length] = c;
-    return result;
-  }
-  
-  
-  
-  static private char[] findRepeatingChars(char[] chars) {
-    char[] result = new char[0];
-    int[] map = new int[256];
-
-    for (char c : chars) {
-      map[c]++;
-      if (2 == map[c])
-        result = addToCharArray(c, result);
-    }
+    if(a.length!= b.length)
+      return false;
     
-    return result;
+    int[] mapA = new int[256];
+    int[] mapB = new int[256];
+    for(int i=0;i<a.length;i++) {
+      mapA[a[i]]++;
+      mapB[b[i]]++;
+    }
+    return Arrays.equals(mapA, mapB);
   }
   
-  static private boolean isAnagram(char[] s1, char[] s2) {
-
-    int[] map1 = new int[256];
-    int[] map2 = new int[256];
-
-    for (char c : s1)
-      map1[c]++;
-    for (char c : s2)
-      map2[c]++;
-
-    for (int i = 0; i < 256; i++)
-      if (map1[i] != map2[i])
-        return false;
-
-    return true;
-  }
+  
+  
 
 }
 
